a11cf28ab70f6100fc83b6f2ab3337149643bf6e
Cody O'Donnell <codonnell@codys-mbp.attlocal.net>
2019-03-11 21:51:44 -0400

    First commit

---
+++ Assets/Scripts/temperatureControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class temperatureControl : MonoBehaviour
+{
+    int minValue = 0, maxValue = 100, variationAmount = 2;
+    public Slider tempSlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tempSlider = this.gameObject.GetComponent<Slider>();
+        tempSlider.minValue = minValue;
+        tempSlider.maxValue = maxValue;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("down"))
+        {
+            tempSlider.value -= variationAmount;
+        }
+        else if (Input.GetKey("up"))
+        {
+            tempSlider.value += variationAmount;
+        }
+    }
+}
